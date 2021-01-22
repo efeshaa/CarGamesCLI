b1469ffb073e1b396d93e0a9d2701a3b04c10eca
Sakhiy Hibban <sakhiyhibban123@outlook.com>
2021-01-22 20:29:10 +0700

    added car selection feature (still crappy tho and to be fixed later)

--- CarGamesCLI/Source.cpp
+++ CarGamesCLI/Source.cpp
@@ -19,83 +19,126 @@ int main()
 
 	std::cout << "Welcome to simple car games (still on development)";
 
+	std::string vehicleSelection[2] = { "McLaren 765lt", "Mercedes-AMG GT Black Series" };
+	bool vehicleSelected = false;
+	int selectedVehicle;
+
 	while (true)
 	{
-		std::cout << std::endl << "command: ";
-		std::cin >> commands;
+		
 
-		if (commands == "help")
+		// If vehicle isn't selected you prompted to select the vehicle first
+		while (!vehicleSelected)
 		{
-			std::cout << "Command list:\n1. start\t-> start the car\n2. stop\t\t-> stop the car\n3. status\t-> see car status (on or off)\n4. exit\t\t-> exit the console\n5. lap\t\t-> do lap on track\nThis cli is still on developemnt";
+			std::cout << "Vehicle selection:\n" << vehicleSelection[0] << std::endl << vehicleSelection[1] << "\nSelect (1/2): " ;
+			std::cin >> selectedVehicle;
+			vehicleSelected = true;
+			selectedVehicle--;
 		}
-		else if (commands == "start")
-		{
-			if (!carStatus)
-			{
-				carStatus = true;
-				std::cout << "Car started";
-			}
-			else if (carStatus)
-			{
-				std::cout << "Car already started use stop to stop the car";
-			}
-		}
-		else if (commands == "stop")
-		{
-			if (!carStatus)
-			{
-				std::cout << "Car already stopped use start to start the car";
-			}
-			else if (carStatus)
-			{
-				carStatus = false;
-				std::cout << "Car stopped";
-			}
-		}
-		else if (commands == "exit")
-		{
-			if (carStatus)
-			{
-				std::cout << "You must turn off your car...";
-			}
-			else
-			{
-				return 0;
-			}
-		}
-		else if (commands == "status")
-		{
-			if (carStatus)
-			{
-				std::cout << "Car turned on";
-			}
-			else
-			{
-				std::cout << "Car turned off";
-			}
-		}
-		else if (commands == "lap")
-		{
-			if (carStatus)
-			{
-				int lap;
-				std::cout << "How many lap you want to do?(1 lap = 30 second): ";
-				std::cin >> lap;
 
-				for (int i = 1; i <= lap; i++) {
-					std::cout << "Lap " << i << std::endl;
-					int lapTime = rand() % 5000 + 20000;
-					Sleep(lapTime);
-					std::cout << "Lap time: " << lapTime << " Milliseconds" << std::endl;
+		// continue with the cli after the vehicle selected
+		if (vehicleSelected)
+		{
+			std::cout << std::endl << "command: ";
+			std::cin >> commands;
+
+			// show command from the user
+			if (commands == "help")
+			{
+				std::cout << "Command list:\n1. start\t-> start the car\n2. stop\t\t-> stop the car\n3. status\t-> see car status (on or off)\n4. exit\t\t-> exit the console\n5. lap\t\t-> do lap on track\nThis cli is still on developemnt";
+			}
+			
+			//-------------------------------------- START --------------------------------------
+			else if (commands == "start")
+			{
+				if (!carStatus)
+				{
+					carStatus = true;
+					std::cout << "Car started";
+				}
+				else if (carStatus)
+				{
+					std::cout << "Car already started use stop to stop the car";
 				}
 			}
-			else {
-				std::cout << "You must start your car!";
+
+			//-------------------------------------- STOP --------------------------------------
+			else if (commands == "stop")
+			{
+				if (!carStatus)
+				{
+					std::cout << "Car already stopped use start to start the car";
+				}
+				else if (carStatus)
+				{
+					carStatus = false;
+					std::cout << "Car stopped";
+				}
 			}
-		}
-		else
-		{
-			std::cout << commands << ": command not found use help to get help";
+
+			//-------------------------------------- EXIT THE TERMINAL --------------------------------------
+			else if (commands == "exit")
+			{
+				if (carStatus)
+				{
+					std::cout << "You must turn off your car...";
+				}
+				else
+				{
+					return 0;
+				}
+			}
+
+			//-------------------------------------- SHOW VEHICLE STATUS --------------------------------------
+			else if (commands == "status")
+			{
+				std::cout << vehicleSelection[selectedVehicle] << std::endl;
+				if (carStatus)
+				{
+					std::cout << "Car turned on";
+				}
+				else
+				{
+					std::cout << "Car turned off";
+				}
+			}
+
+			//-------------------------------------- DO SOME LAP --------------------------------------
+			else if (commands == "lap")
+			{
+				if (carStatus)
+				{
+					int lap;
+					std::cout << "How many lap you want to do?(1 lap = 30 second): ";
+					std::cin >> lap;
+
+					for (int i = 1; i <= lap; i++) {
+						if (selectedVehicle = 0)
+						{
+							std::cout << "Lap " << i << std::endl;
+							int lapTime = rand() % 5000 + 20000;
+							Sleep(lapTime);
+							std::cout << "Lap time: " << lapTime << " Milliseconds" << std::endl;
+						}
+						else if (selectedVehicle = 1)
+						{
+							std::cout << "Lap " << i << std::endl;
+							int lapTime = rand() % 4000 + 19000;
+							Sleep(lapTime);
+							std::cout << "Lap time: " << lapTime << " Milliseconds" << std::endl;
+						}
+					}
+				}
+				else {
+					std::cout << "You must start your car!";
+				}
+			}
+
+			//-------------------------------------- SHOW ERROR --------------------------------------
+			else
+			{
+				std::cout << commands << ": command not found use help to get help";
+			}
 		}
 	}
 	return 0;
